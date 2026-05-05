83d65ecf9b954779f0aba488cf17867936ca95c7
akashsarker99 <akashsarker2003@gmail.com>
2026-05-06 03:13:06 +0600

    course detail section added

--- src/components/PopularCourse.jsx
+++ src/components/PopularCourse.jsx
@@ -6,17 +6,17 @@ const PopularCourse = ({course}) => {
     console.log(course)
     return (
         <div>
-            <div className="card bg-base-100 shadow-md border border-gray-300 h-[400px]">
-  <figure>
-      <Image className='rounded-xl' src={course.image} width={400} height={200} alt={course.title}></Image>
+            <div className="card bg-base-100 shadow-md border border-gray-300 h-full">
+  <figure className='overflow-hidden'>
+      <Image className='rounded-xl' src={course.image} height={300} width={400}  alt={course.title}></Image>
   </figure>
-  <div className="card-body">
+  <div className='space-y-2'>
     <h2 className="text-lg font-bold">{course.title}</h2>
     <p className='font-semibold'>Instructor: <span className='font-bold'>{course.instructor}</span></p>
     <p className='font-semibold'>Rating: <span className='font-bold'>{course.rating}</span></p>
     <p className='font-semibold'>Category: <span className='font-bold'>{course.category}</span></p>
     <div className="card-actions justify-end">
-      <Link href={'/coursedetails'}><button className="btn btn-neutral">Show Details</button></Link>
+      <Link href={`/allcourse/${course.id}`}><button className="btn btn-neutral">Show Details</button></Link>
     </div>
   </div>
 </div>
