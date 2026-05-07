a65c4eeaaf6d1d0e93f5130f8c021d67d166852c
akashsarker99 <akashsarker2003@gmail.com>
2026-05-07 12:25:20 +0600

    aa

--- src/lib/getfiles.js
+++ src/lib/getfiles.js
@@ -1,13 +1,13 @@
 
 
 export const getCourses =  async()=>{
-  const res = await fetch('http://localhost:3000/data.json');
+  const res = await fetch('http://localhost:3000/data.json',{cache:"no-store"});
   const data = await res.json();
   return data;
 }
 
 export const getInstructors = async()=>{
-  const res = await fetch('http://localhost:3000/instructors.json');
+  const res = await fetch('http://localhost:3000/instructors.json',{cache:"no-store"});
   const data = await res.json();
   return data;
 
