c03e7b7fa2dafc644293fda5161ffa764886989a
akashsarker99 <akashsarker2003@gmail.com>
2026-05-07 12:30:40 +0600

    vercel deployment

--- src/lib/getfiles.js
+++ src/lib/getfiles.js
@@ -1,13 +1,13 @@
 
 
 export const getCourses =  async()=>{
-  const res = await fetch('http://localhost:3000/data.json',{cache:"no-store"});
+  const res = await fetch('https://online-learning-a8-pl5q.vercel.app/data.json',{cache:"no-store"});
   const data = await res.json();
   return data;
 }
 
 export const getInstructors = async()=>{
-  const res = await fetch('http://localhost:3000/instructors.json',{cache:"no-store"});
+  const res = await fetch('https://online-learning-a8-pl5q.vercel.app/instructors.json',{cache:"no-store"});
   const data = await res.json();
   return data;
 
