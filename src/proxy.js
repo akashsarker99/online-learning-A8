147547e41f044e3a3e4049deb266098c81de2cd3
akashsarker99 <akashsarker2003@gmail.com>
2026-05-12 18:27:02 +0600

    wrote proxy again

--- src/proxy.js
+++ src/proxy.js
@@ -4,9 +4,9 @@ import { NextResponse } from "next/server";
 import { auth } from "./lib/auth";
 import { headers } from "next/headers";
 
-export async function middleware(request) {
+export async function proxy(request) {
      const session = await auth.api.getSession({
-        headers: request.headers()
+        headers: await headers()
     })
 
    if(session){
