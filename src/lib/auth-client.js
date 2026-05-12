d34cee2af17ab61570b804515a6d2900d82449e5
akashsarker99 <akashsarker2003@gmail.com>
2026-05-12 17:26:42 +0600

    still trying to fix error

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://online-learning-a8-pl5q.vercel.app"
+    baseURL: "https://online-learning-a8-bvll.vercel.app"
 })
 
 export const { signIn, signUp, signOut, useSession, updateUser } = createAuthClient()
